db97b433fd17df219b1ad778e7bbe9a3129cefe9
Alex Naumchenko <alex@devloft.com>
2015-04-27 13:54:17 -0400

    Empty cookbook, ground-zero.

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'spree'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures spree'
+long_description 'Installs/Configures spree'
+version          '0.1.0'
